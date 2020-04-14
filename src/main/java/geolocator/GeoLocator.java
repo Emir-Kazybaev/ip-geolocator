7b180aa4044d975cce74e4ad117d3ca1e5721170
Emir-Kazybaev <61088019+Emir-Kazybaev@users.noreply.github.com>
2020-04-14 14:29:33 +0200

    Added Log4j 2 and SLF4J

--- src/main/java/geolocator/GeoLocator.java
+++ src/main/java/geolocator/GeoLocator.java
@@ -8,9 +8,14 @@ import com.google.gson.Gson;
 
 import com.google.common.net.UrlEscapers;
 
+import com.sun.tools.javac.Main;
 import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class GeoLocator {
+
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static final String GEOLOCATOR_SERVICE_URI = "http://ip-api.com/json/";
 
@@ -19,6 +24,7 @@ public class GeoLocator {
     public GeoLocator() {}
 
     public GeoLocation getGeoLocation() throws IOException {
+        logger.debug("Get GeoLocation");
         return getGeoLocation(null);
     }
 
@@ -26,19 +32,25 @@ public class GeoLocator {
         URL url;
         if (ipAddrOrHost != null) {
             ipAddrOrHost = UrlEscapers.urlPathSegmentEscaper().escape(ipAddrOrHost);
+            logger.debug("IP Address or Host is " + ipAddrOrHost);
             url = new URL(GEOLOCATOR_SERVICE_URI + ipAddrOrHost);
+            logger.info("url: " + url);
         } else {
             url = new URL(GEOLOCATOR_SERVICE_URI);
+            logger.info("url: " + url);
         }
         String s = IOUtils.toString(url, "UTF-8");
+        logger.debug("Converting URL to UTF-8 string");
         return GSON.fromJson(s, GeoLocation.class);
     }
 
     public static void main(String[] args) throws IOException {
         try {
             String arg = args.length > 0 ? args[0] : null;
+            logger.debug("Checking Command Line argument");
             System.out.println(new GeoLocator().getGeoLocation(arg));
         } catch (IOException e) {
+            logger.error("This is IOException message: " + e.getMessage());
             System.err.println(e.getMessage());
         }
     }
